0176cc8c8adae7ef4e8da8dcfc058a62404c2ada
Icaro Rocha <icaro.rocha@epitech.eu>
2020-05-06 17:11:25 +0200

    daily commit 17h

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Getting started</div>
+
+                <div class="card-body">
+                    Please login or register to get access to our full content.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Auth::routes(['verify' => true]);
+Route::get('/', "IndexController@showIndex");
+Route::get('/home', 'HomeController@index')->name('home')->middleware('verified');
+// Route::get('/users/create', "UserController@create");
+// Route::post('users/{user}/edit', "UserController@edit");
